3c34744842173b969e2396ef60fd59300520f883
Tobias Anker <tobias.anker@kitsunemimi.moe>
2019-10-16 19:38:37 +0200

    renamings in build-script

--- build.sh
+++ build.sh
@@ -17,17 +17,17 @@ mkdir -p $RESULT_DIR
 function build_kitsune_lib_repo () {
 	REPO_NAME=$1
 
-	# create build directory for libKitsuneProjectCommon and go into this directory
-	LIB_KITSUNE_PROJECT_COMMON_DIR="$BUILD_DIR/$REPO_NAME"
-	mkdir -p $LIB_KITSUNE_PROJECT_COMMON_DIR
-	cd $LIB_KITSUNE_PROJECT_COMMON_DIR
+	# create build directory for repo and go into this directory
+	REPO_DIR="$BUILD_DIR/$REPO_NAME"
+	mkdir -p $REPO_DIR
+	cd $REPO_DIR
 
-	# build libKitsuneProjectCommon library with qmake
+	# build repo library with qmake
 	/usr/lib/x86_64-linux-gnu/qt5/bin/qmake "$PARENT_DIR/$REPO_NAME/$REPO_NAME.pro" -spec linux-g++ "CONFIG += optimize_full"
 	/usr/bin/make -j4
 
 	# copy build-result and include-files into the result-directory
-	cp -d $LIB_KITSUNE_PROJECT_COMMON_DIR/src/$REPO_NAME.so.* $RESULT_DIR/
+	cp -d $REPO_DIR/src/$REPO_NAME.so.* $RESULT_DIR/
 	cp -r $PARENT_DIR/$REPO_NAME/include $RESULT_DIR/
 }
 
